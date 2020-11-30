ce4f512f85b41fdb6b535bf9fad8691b2cf45f4e
yunhalee05 <yunhalee0506@naver.com>
2020-11-30 21:59:40 +0900

    프로그래머스 문제

---
+++ 프로그래머스/코딩테스트연습/탐욕법_Lv.2_큰_수_찾기(꼭_다시풀기).cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string number, int k) {
+    string answer = "";
+    int len = number.length() - k;
+    int idx = -1;
+    int j = 0;
+    for (int i = 0; i < len; i++)
+    {
+        char max = '0';
+        for (int j = idx + 1; j <= i + k; j++)
+        {
+            if (max < number[j])
+            {
+                max = number[j];
+                idx = j;
+            }
+        }
+        answer += max;
+    }
+
+
+    return answer;
+}
